3161167da3b6a9310e3b2fe613a450caf9aec957
Derek Griffith <dgriffith@csir.co.za>
2015-12-23 16:21:27 +0200

    Update to morticia/__init__.py

--- morticia/__init__.py
+++ morticia/__init__.py
@@ -1,4 +1,11 @@
-#Create a global units registry
+# Create a global units registry
 from pint import UnitRegistry
 ureg = UnitRegistry()
-Q_=ureg.Quantity
+Q_= ureg.Quantity
+def U_(units):
+    """ Returns a pint.Quantity of magnitude one with the given units
+
+    :param units: A string providing a unit of measure in the pint unit registry
+    :return: A pint.Quantity object with a magnitude of 1 and the specified units.
+    """
+    return Q_(1.0, units)
