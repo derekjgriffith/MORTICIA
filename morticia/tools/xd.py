83eee6a1dae49fb1106a78c1463909b7c34adc95
Derek Griffith <dgriffith@csir.co.za>
2015-12-27 14:10:19 +0200

    xd.xd_harmonise_interp function seems to be working.

--- morticia/tools/xd.py
+++ morticia/tools/xd.py
@@ -10,8 +10,9 @@ __author__ = 'DGriffith'
 import numpy as np
 import xray
 from .. import ureg, Q_, U_
-from scipy.interpolate import RegularGridInterpolator
+from scipy.interpolate import RegularGridInterpolator, interpn
 import warnings
+from operator import mul, add
 
 def xd_identity(np_vector, axis_name):
     """ Create an identity xray.DataArray. That is, a DataArray vector in which both the values and axis
@@ -62,21 +63,23 @@ def xd_harmonise_interp(xd_list):
         # Create the linear interpolator
         interpolator = RegularGridInterpolator([xd_arr[axis].values for axis in xd_arr.dims], xd_arr.values,
                                                method='linear', bounds_error=False, fill_value=0.0)
-        interp_vals = interpolator(np.array([index_vals[axis] for axis in xd_arr.dims]).T)
-        # TODO : Reconstruct the metadata
+        merged_coordinates = np.meshgrid(*[index_vals[axis] for axis in xd_arr.dims], indexing='ij')
+        interp_vals = interpolator(tuple(merged_coordinates))
         # reconstruct the xray.DataArray with interpolated data
-        xd_arr = xray.DataArray(interp_vals, [(axis, index_vals[axis]) for axis in xd_arr.dims], name=xd_arr.name,
-                                attrs=xd_arr.attrs)
-        xd_return_list.append(xd_arr)
+        xd_arr_interp = xray.DataArray(interp_vals, [(axis, index_vals[axis]) for axis in xd_arr.dims],
+                                       name=xd_arr.name, attrs=xd_arr.attrs)
+        xd_arr_interp.attrs = xd_arr.attrs  # transfer the attributes verbatim
+        xd_return_list.append(xd_arr_interp)
 
-    # There may be axes not present in a specific DataArray. These are omitted for that DataArray and
-    # simply allowed to broadcast when performing operations with other DataArrays
+        # There may be axes not present in a specific DataArray. These are omitted for that DataArray and
+        # simply allowed to broadcast when performing operations with other DataArrays
     return xd_return_list
 
 def xd_harmonised_product(xd_list):
     """ Compute the harmonised product of a number of N-dimensional data arrays.
         The DataArrays are interpolated onto a common set of coordinates and then the product of the DataArrays
-        is computer, returning a single DataArray with merged attributes. Unit mismatches are
+        is computer, returning a single DataArray with merged attributes. Unit mismatches are flagged with warnings.
+
     :param xd_list: List/tuple of xray.DataArray objects to be multiplied
     :return: Product of xray.DataArray objects with merged attributes
     """
@@ -87,12 +90,14 @@ def xd_harmonised_product(xd_list):
         metadata.update(xd_arr.attrs)
         for axis in xd_arr.dims:
             if not axis in unit_dict:
-                unit_dict[axis] = axis.attrs[axis + '_units']
-            elif unit_dict[axis] != axis.attrs[axis + '_units']:
+                unit_dict[axis] = xd_arr.attrs[axis + '_units']
+            elif unit_dict[axis] != xd_arr.attrs[axis + '_units']:
+                # TODO : COnsider throwing a unit mismatch error, or converting to desired units with pint
                 warnings.warn('Unit mismatch found when taking xray.DataArray harmonised product.')
-    xd_product = xd_harmonise_interp(xd_list)
+    xd_factors = xd_harmonise_interp(xd_list)
+    xd_product = reduce(mul, xd_factors)  # take the product by reducing the list using the mul operator
+    xd_product.attrs = metadata
     return xd_product
-
 
 
 def check_convert_units(value_with_units, preferred_units):
@@ -115,7 +120,7 @@ def xd_check_convert_units(xd, axis_name, preferred_units):
 
     :param xd: An xray.DataArray object having an axis called `axis_name` and a value in the `attrs` dictionary
     :param preferred_units: A string providing the preferred units that can be passed to `pint`
-    :return: A xray.DataArray, inwhich the values in the named axis have been converted to the preferred units
+    :return: A xray.DataArray, in which the values in the named axis have been converted to the preferred units
         The `axis_name_units` field is also updated.
     """
 
@@ -124,6 +129,3 @@ def xd_check_convert_units(xd, axis_name, preferred_units):
     Q_values = Q_values.to(preferred_units)
     xd[axis_name] = Q_values.magnitude
     xd.attrs[axis_name + '_units'] = preferred_units
-
-
-
