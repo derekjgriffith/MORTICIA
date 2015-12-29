48a33badea97debacdfdaf397d87816f9d9102a8
Derek Griffith <dgriffith@csir.co.za>
2015-12-29 13:17:07 +0200

    Started building long_name dict in moglo.py.

--- morticia/moglo.py
+++ morticia/moglo.py
@@ -7,3 +7,35 @@ __author__ = 'DGriffith'
 
 # MORTICIA globals and utility functions
 
+
+# The following are the long names that will be provided
+long_names = {
+    'wvl': 'Wavelength',
+    'wvn': 'Wavenumber',
+    'trn': 'Transmission',
+    'rad': 'Radiance',
+    'irr': 'Irradiance',
+    'fldx': 'Field Position in x',
+    'fldy': 'Field Position in y',
+    'fldz': 'Defocus',
+    'spf': 'Spatial Frequency',
+    'mtf': 'Modulation Transfer Function',
+    'otf': 'Optical Transfer Function',
+    'ptf': 'Phase Transfer Function',
+    'fno': 'Focal Ratio',
+    'efl': 'Effective Focal Length',
+    'lum': 'Luminance',
+    'ill': 'Illuminance',
+    'flux': 'Optical Flux',
+    'flo': 'Field Orientation',  # Typically horizontal/vertical, across/along track, sagittal/tangential
+    'obs': 'Obscuration Ratio',
+    'pitchx': 'Pixel Pitch in x',
+    'pitchy': 'Pixel Pitch in y',
+    'asr': 'Absolute Spectral Response',
+    'sqe': 'Spectral Quantum Efficiency'
+}
+
+# The following are intended to be the standard names as per the Climate and Forecast (CF) convention
+standard_names = {
+
+}
