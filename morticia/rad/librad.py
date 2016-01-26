1c680b459d6c20bfb434a0c5f2e2dfbdd36d0a27
Derek Griffith <dgriffith@csir.co.za>
2016-01-26 09:51:22 +0200

    Reverted debug self.n_wvl == '?'.

--- morticia/rad/librad.py
+++ morticia/rad/librad.py
@@ -752,7 +752,7 @@ class Case(object):
                 self.n_levels_out = len(np.unique(fluxdata[:,0]))
             fluxdata = fluxdata.reshape((self.n_levels_out, -1, linecount), order='F')
         elif fields[0] == 'wvl' or fields[0] == 'wvn':  # wavelength/wavenumber is the primary variable
-            if self.n_levels_out == 0 or self.n_wvl == '?':  # Don't know number of output levels or number of wavelengths
+            if self.n_levels_out == 0:  # Don't know number of output levels
                 self.n_wvl = len(np.unique(fluxdata[:,0]))  # Try to determine number of wavelengths/wavenumbers
                 fluxdata = fluxdata.reshape((self.n_wvl, -1, linecount), order='F')
             else:
