76c3dc5daef601ab88ae00c63e44288fd5c30b7e
Ari Ramkilowan - Laptop <ari.ramkilowan@gmail.com>
2016-01-29 16:12:58 +0200

    conf.py changed to include read_the_docs theme and make use of logo, although at this stage the logo is just an image directive. the html_logo option in conf.py does not work yet

--- morticia/doc/conf.py
+++ morticia/doc/conf.py
@@ -14,12 +14,14 @@
 
 import sys
 import os
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../sensor'))
 sys.path.insert(0, os.path.abspath('../rad'))
+sys.path.insert(0, os.path.abspath('../demonstration'))
 
 # -- General configuration ------------------------------------------------
 
@@ -52,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MORTICIA'
-copyright = u'2015, D Griffith, A Ramkilowan'
+copyright = u'2016, D Griffith, A Ramkilowan'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -106,8 +108,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'classic'
-
+# html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
@@ -115,7 +117,7 @@ html_theme = 'classic'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 #html_title = None
@@ -125,7 +127,7 @@ html_theme = 'classic'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'D:/GitHub/morticia/morticia/doc/_build/html/_static/MORTICIAlogo5.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
