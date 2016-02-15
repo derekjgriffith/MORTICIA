2f1c0957a36f63a5a4354b5a445873f4a48ad579
Derek Griffith <dgriffith@csir.co.za>
2016-02-15 13:44:56 +0200

    Work on beachball target.

--- morticia/scene/target.py
+++ morticia/scene/target.py
@@ -15,8 +15,9 @@ class Target(object):
     """
 
     def __init__(self, name, desc, n_pix, anti_aliasing ):
-        """Target constructor
-        The target is constructed using the characteristic dimension....
+        """ Target constructor
+            The target is constructed using the characteristic dimension....
+
         :param name: Name of target
         :param desc: Description of target
         :param n_pix: The number pixels at which to render target
@@ -42,22 +43,22 @@ class Target(object):
         return self.anti_aliasing
 
 class GreyscaleLambertianBeachballTarget(Target):
-    """
-        This class inherits the generic(abstract) Target class and adds to
+    """ This class inherits the generic(abstract) Target class and adds to
         it specialised methods specific to this type of target. An instantiation
-        of this class will render a greyscale beachball.
+        of this class will render a greyscale beachball. This target is a spherical,lambertian reflector
+        that is neutral (grey) across all wavelengths.
     """
-    def __init__(self, diam= 1.0, n_sector_pairs= 12, min_refl= 0.0, max_refl= 1.0, n_pix= 128, anti_aliasing= 1):
-        """
+    def __init__(self, diam=1.0, n_sector_pairs=12, min_refl=0.0, max_refl=1.0, n_pix=128, anti_aliasing=1, zero_padding):
+        """ Create a greyscale lambertian beachball target.
 
-        :param diam:
-        :param n_sector_pairs:
-        :param normal_vector
-        :param min_refl
-        :param max_refl
-        :param n_pix
-        :param anti_aliasing is set to 1 by default, this means there is no oversampling. should be an integer equal 
-                to how many times more resolution should the target have in the image plane relative to n_pix
+        :param diam: Diameter of the target in m
+        :param n_sector_pairs: Number of dark/light sector pairs
+        :param min_refl: Reflectance of the dark sectors
+        :param max_refl: Reflectance of the light sectors
+        :param n_pix: Number of pixels (horizontal and vertical) to render target
+        :param anti_aliasing: is set to 1 by default, this means there is no oversampling. Can be set to an integer
+            value to oversample the target and then downsample to n_pix in order to reduce aliasing effects (jagged
+            edges) along the sector boundaries.
         :return:
         """
         Target.__init__(self, 'BeachBall', 'Grey Spherical Lambertian', n_pix= n_pix, anti_aliasing= anti_aliasing)
@@ -75,50 +76,48 @@ class GreyscaleLambertianBeachballTarget(Target):
 
     def __str__(self):
         printString = """ The target is a %s %s with %i sectors, rendered with %i pixels and oversampled by a factor %i."""         % (self.desc, self.name, self.n_sector_pairs, self.n_pix, self.anti_aliasing)
-
         return str(printString)
     
     def get_target(self):
-        
+        """ Build a reflectance image of the target and calculate surface normals at every pixel
+
+        """
+        # Set up pixel relative x-coodinates
         x = np.linspace(-1.0, 1.0, self.n_pix * self.anti_aliasing)
-
+        # Meshgrid the x and y coordinates as the same
         x, y = np.meshgrid(x, x)
-
+        # Calculate the polar coordinate theta of each pixel
         theta = np.arctan2(y, x)
-              
+        # Produce a siemens star target with the given number of spokes/sectors
         siemans_star = np.cos(self.Nsector_pairs * theta)
-
+        # Create an array that will become a target mask
         mask = np.ones(siemans_star.shape)
-        
-        siemans_star =  np.sign(siemans_star)/2.0 + 0.5        
-        
+        # Make the mask binary by taking the sign of the siemens star
+        siemans_star =  np.sign(siemans_star)/2.0 + 0.5
         # Set all value outside the unit circle to the new value
-        
         circle = np.sqrt(x**2 + y**2) > 1.0
-
         mask[circle] = 0.0
-
-        
         # downsample if aliasing requested
         if self.anti_aliasing > 1.0:
-
             the_filter = np.ones((self.anti_aliasing, self.anti_aliasing)) / self.anti_aliasing**2
-            siemans_star = sg.convolve2d(siemans_star, the_filter, mode = 'same')
-            mask = sg.convolve2d(mask, the_filter, mode = 'same') 
-
+            # Apply 2D filter convolution
+            siemans_star = sg.convolve2d(siemans_star, the_filter, mode = 'full')
+            # Convole the mask as well
+            mask = sg.convolve2d(mask, the_filter, mode = 'full')
             siemans_star = siemans_star[0.0::self.anti_aliasing, ::self.anti_aliasing]
             mask = mask[0.0::self.anti_aliasing, ::self.anti_aliasing]
-            
         x = np.linspace(-1.0, 1.0, siemans_star.shape[0])
         y = np.linspace(-1.0, 1.0, siemans_star.shape[1]) 
         x, y = np.meshgrid(x, y)            
-            
+        # Create blank list of normal vector components
         normal_vector_list = [[],[],[]]
-            
         normal_vector_list[0] = x
         normal_vector_list[1] = y
         normal_vector_list[2] = (csqrt(1.0 - x**2 - y**2)).real
-
+        # Save reflectance map, normal vectors and mask in self
+        self.normal_vector_list = normal_vector_list
+        self.siemens_star = siemans_star
+        self.mask = mask
         return normal_vector_list, mask, siemans_star
 
 test = GreyscaleLambertianBeachballTarget(Npix=128*4, anti_aliasing=1)
@@ -156,7 +155,7 @@ def render_target(self, vza, svaz, normal_vector, rad_env, rad_fields):
         
         targ_SHREM = rad_env.compilation[0].rad_fields[0] *sph_fit
         targ_edir = rad_env.compilation[0].edir
-    else 
+    else:
     
         pass # log an error / warning here
         
