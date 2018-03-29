5ffb2671ac681eb7557e1b1f3fcc43cd2eefd2f1
Derek Griffith <dgriffith@csir.co.za>
2018-03-29 16:20:13 +0200

    Fixes to basic __init__ of Mitsuba object.

--- morticia/scene/mortsuba.py
+++ morticia/scene/mortsuba.py
@@ -26,16 +26,23 @@ __author__ = 'DGriffith'
 Class for Mitsuba support
 """
 #import lxml
-#import mitsuba
 
+# The following is messy, but nothing else seems to work
+import os, sys
+# NOTE: remember to specify paths using FORWARD slashes (i.e. '/' instead of
+# '\' to avoid pitfalls with string escaping)
+# Configure the search path for the Python extension module
+sys.path.append('D:/Projects/MORTICIA/Render/Mitsuba 0.5.0/python/2.7')
+# Ensure that Python will be able to find the Mitsuba core libraries
+os.environ['PATH'] = 'D:/Projects/MORTICIA/Render/Mitsuba 0.5.0' + os.pathsep + os.environ['PATH']
+import mitsuba
 class Mitsuba(object):
     """ The Mitsuba class encapsulates the information provided in a Mitsuba scene file (.xml).
-    This class contains the information in the scene file in an acesssible format for the purpose
+    This class contains the information in the scene file in an accessible format for the purpose
     of manipulating Mitsuba scenes, writing the updated scene file and performing rendering
     in a multiprocessing environment.
 
     """
-    import mitsuba
     def __init__(self, scenefile=None, paramMap=None, scenefolder=None):
         """
 
@@ -46,11 +53,10 @@ class Mitsuba(object):
         which to search for the scene can also be provided (string or list of strings).
         :return:
         """
-        from mitsuba.core import *
-        from mitsuba.render import SceneHandler
+
         # Set up the file resolver for this Mitsuba object
         # Get a reference to the threads file resolver
-        self.fileResolver = Thread.getThread().getFileResolver()
+        self.fileResolver = mitsuba.core.Thread.getThread().getFileResolver()
         if scenefolder is not None:  # the user input something
             if scenefolder:  #  not blank
                 if isinstance(scenefolder, basestring):
@@ -58,14 +64,17 @@ class Mitsuba(object):
                 else:  # assume a list of strings
                     for path in scenefolder:
                         self.fileResolver.appendPath(path)
-
+        theparamMap = mitsuba.core.StringMap()
+        if paramMap is not None:  # dict provided, convert to StringMap
+            for param, value in paramMap.iteritems():
+                theparamMap[param] = value
         # Read in the scene file if requested
         if scenefile is not None:
             if not scenefile:
                 # Open a dialog to get the filename
                 import easygui
-                filename = easygui.fileopenbox(msg='Please select a Mitsuba scene file.', filetypes=["*.xml"])
+                scenefile = easygui.fileopenbox(msg='Please select a Mitsuba scene file.', filetypes=["*.xml"])
             # Load the scene
-            self.scene = Scenehandler.loadScene(
-                           self.fileResolver.resolve(scenefile), paramMap)
+            self.scene = mitsuba.render.SceneHandler.loadScene(
+                           self.fileResolver.resolve(scenefile), theparamMap)
 
